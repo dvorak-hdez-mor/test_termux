392106b3bec75aef172732f2d502729b62b31875
Miguel Hernandez <miguel.hernandezmor@alumno.buap.mx>
2021-06-16 17:45:33 -0500

    Clase añadida

--- prueba.py
+++ prueba.py
@@ -1,2 +1,18 @@
-print("Hola")
-print(f"Menú")
+class Coche:
+    def __init__(self, nombre):
+        self.__nombre = nombre
+    
+    @property
+    def name(self):
+        return self.__nombre
+    
+    @name.setter
+    def name(self, nombre):
+        self.__nombre = nombre
+        print("Nombre modificado".center(30, "-"))
+
+coche1 = Coche("Audi")
+print(coche1.name)
+
+coche1.name = "Volkswagen"
+print(coche1.name)
